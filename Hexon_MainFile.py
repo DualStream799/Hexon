ca55ce9115418039a9bab1ace02bf498d118ffd2
DualStream799 <dualstream799@gmail.com>
2017-11-16 18:23:33 -0200

    New images added, planet sprite rotating correctly, planetary trajectory simulations used to calculate Barrier complete

--- Hexon_MainFile.py
+++ Hexon_MainFile.py
@@ -84,6 +84,8 @@ game_page_purple = pygame.image.load(r'.\Images\Background\game_purple_small.png
 but_pressed = pygame.image.load(r'.\Images\Buttons\Small\hex_but_press_all.png')
 but_pressed_big = pygame.image.load(r'.\Images\Buttons\Small\hex_but_press_play.png')
 but_play = pygame.image.load(r'.\Images\Buttons\Small\hex_but_play.png')
+but_play_base = pygame.image.load(r'.\Images\Buttons\Small\hex_but_play_base.png')
+
 # but_back_left = pygame.image.load(r'.\Images\Buttons\hex_but_back_left.png')
 but_back_right = pygame.image.load(r'.\Images\Buttons\hex_but_back_right.png')
 but_close = pygame.image.load(r'.\Images\Buttons\Small\hex_but_close_small.png')
@@ -115,7 +117,7 @@ planet_original = pygame.image.load(r'.\Images\Objects\Planets\planet_original.p
 # ----------------------------------- POSITIONS ------------------------------------------------ #
 
 # Buttons:
-but_play_pos = [[int((display_height - 165)/2), int((display_width - 180)/2)], [int((display_height + 165)/2), int((display_width + 180)/2)]]
+but_play_pos = [[int((display_height - 180)/2), int((display_width - 180)/2)], [int((display_height + 165)/2), int((display_width + 180)/2)]]
 but_close_pos = [[169, 146], [169 + 109, 146 + 120]]
 but_volon_pos = [[338, 243], [338 + 106, 243 + 122]]
 but_voloff_pos = [[338, 243], [338 + 106, 243 + 122]]
@@ -131,6 +133,10 @@ but_contact_pos = [[2, 243], [2 + 109, 243 + 120]]
 
 # Planets:
 planet_pos = [[int((display_height - 121)/2), int((display_width - 121)/2)], [int((display_height + 121)/2), int((display_width + 121)/2)]]
+
+# -------------------------------- ROTATION SPEED ------------------------------------------------ #
+
+planet_speed = 1
 
 # ------------------------------- OBJECTS CLASSES --------------------------------------------- #
 
@@ -201,22 +207,34 @@ class PlayerOne:
 class PlayerTwo:
 	pass
 
+# ----------------------------- SECONDARY FUNCTIONS -------------------------------------------- #
+
+# Rotate image in fixed center
+def image_rotation_centered(image, angle):
+    orig_rect = image.get_rect()
+    rot_image = pygame.transform.rotate(image, angle)
+    rot_rect = orig_rect.copy()
+    rot_rect.center = rot_image.get_rect().center
+    rot_image = rot_image.subsurface(rot_rect).copy()
+    return rot_image
+
+
 # ------------------------------- MENUS FUNCTIONS ---------------------------------------------- #
 # Main loop
 def HomePage():
+	# Loading all pre set configurations:
+	Volume_on = True
 	# Loop Controler:
 	home_runner = True
 	# Main Loop:
-	while home_runner:
-		# Loading all pre set configurations:
-		Volume_on = True
+	while home_runner:	
 		# Loading Home Page (showing all the elements which compose the menu):
 		screen.blit(home_page_purple, (0, 0))
 		screen.blit(but_close, (but_close_pos[0][0], but_close_pos[0][1]))
-		screen.blit(but_play, (but_play_pos[0][0], but_play_pos[0][1]))
 		screen.blit(but_volon, (but_volon_pos[0][0], but_volon_pos[0][1]))
 		screen.blit(but_profile, (but_profile_pos[0][0], but_profile_pos[0][1]))
 		screen.blit(but_star, (but_star_pos[0][0], but_star_pos[0][1]))
+		screen.blit(but_play, (but_play_pos[0][0], but_play_pos[0][1]))
 
 		# screen.blit(but_config, (but_config_pos[0][0], but_config_pos[0][1]))
 		# screen.blit(but_contact, (but_contact_pos[0][0], but_contact_pos[0][1]))
@@ -245,15 +263,17 @@ def HomePage():
 					GamePage()
 
 				# VOLUME ON BUTTON Clicked:
-				if (mouse_pos_x in range(but_volon_pos[0][0], but_volon_pos[1][0])) and (mouse_pos_y in range(but_volon_pos[0][1], but_volon_pos[1][1])):
+				if (mouse_pos_x in range(but_volon_pos[0][0], but_volon_pos[1][0])) and (mouse_pos_y in range(but_volon_pos[0][1], but_volon_pos[1][1])) and Volume_on == True:
 					screen.blit(but_pressed, (but_volon_pos[0][0], but_volon_pos[0][1]))
 					screen.blit(but_voloff, (but_voloff_pos[0][0], but_voloff_pos[0][1]))
+					Volume_on = False
 					print("VolOn Clicked")
 
 				# VOLUME OFF BUTTON Clicked:
-				if (mouse_pos_x in range(but_voloff_pos[0][0], but_voloff_pos[1][0])) and (mouse_pos_y in range(but_voloff_pos[0][1], but_voloff_pos[1][1])):
+				if (mouse_pos_x in range(but_voloff_pos[0][0], but_voloff_pos[1][0])) and (mouse_pos_y in range(but_voloff_pos[0][1], but_voloff_pos[1][1])) and Volume_on == False:
 					screen.blit(but_pressed, (but_voloff_pos[0][0], but_voloff_pos[0][1]))
 					screen.blit(but_volon, (but_voloff_pos[0][0], but_voloff_pos[0][1]))
+					Volume_on = True
 					print("VolOff Clicked")
 		
 				# PROFILE BUTTON Clicked:
@@ -265,7 +285,8 @@ def HomePage():
 		pygame.display.update()
 		# Frame Rate Update (current rate: 60fps):
 		clock.tick(60)
-							 
+
+
 def PausePage():
 	# Loop Controler:
 	pause_runner = True
@@ -304,7 +325,7 @@ def PausePage():
 		# Frame Rate Update (current rate: 60fps):
 		clock.tick(60)
 
-def RightPage():
+def ProfilePage():
 	pass
 
 
@@ -315,14 +336,15 @@ def LoadingPage():
 def GamePage():
 	# Loop Controler:
 	game_runner = True
+	planet_angle = 0
 	# Game Loop
 	while game_runner:
-		angle = 15
-		image_alpha = 0
 		# Loading Home Page (showing all the elements which compose the menu):
+		rotating_planet = image_rotation_centered(planet_original, planet_angle)
 		screen.blit(game_space, (0, 0))
 		screen.blit(but_pause, (but_pause_pos[0][0], but_pause_pos[0][1])) 
 		screen.blit(planet_original, (planet_pos[0][0], planet_pos[0][1]))
+		screen.blit(rotating_planet, (planet_pos[0][0], planet_pos[0][1]))
 		# screen.blit(loading_page, (0, 0))
 		# time.sleep(10)
 		# Screen Commands:
@@ -356,7 +378,10 @@ def GamePage():
 		pygame.display.update()
 		# Frame Rate Update (current rate: 60fps):
 		clock.tick(60)
-
+		if planet_angle >= 360:
+			planet_angle = 0
+		else:
+			planet_angle += 0.5
 
 
 def GameEnd():
